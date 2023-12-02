d516cc4c53faf261516eed3963eab648ea764257
1Kuznetsov <i.kuznetsov5@g.nsu.ru>
2023-12-02 17:45:06 +0700

    v 1.0

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ import os
 
 
 def runCommand(command):
-    pass
+    if command == 1:
+        return None
+    if command == 2:
+        return moveUp()
+    if command == 3:
+        return moveDown(curDir)
+    if command == 4:
+        return countFiles(pth)
+    if command == 5:
+        return countBytes(pth)
+    if command == 6:
+        return findFiles(trgt, pth)
 
 
 def countFiles(path):
-    pass
+    cnt = 0
+
+    try:
+        for item in os.listdir(path):
+            new_path = os.path.join(path, item)
+            if os.path.isfile(new_path):
+                cnt += 1
+            elif os.path.isdir(new_path):
+                cnt += countFiles(new_path)
+        return cnt
+    except PermissionError:
+        print(None)
 
 
 def main():
