8df53503eb22cfa5f1f9305def6517d84926a505
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-12-03 19:00:22 +0700

    a

--- main.py
+++ main.py
@@ -2,14 +2,63 @@ import os
 import ru_local as ru
 
 
+def acceptCommand():
+    while True:
+        cmnd = int(input(ru.CHOICE))
+        if cmnd > 0 and cmnd < 8:
+            return int(cmnd)
+        else:
+            print(ru.NOT_CHOICE)
+command = acceptCommand()
+print(command)
+
+
+def moveUp():
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    os.chdir(parent_directory)
+    print(ru.CURRENT_DIRECTORY, os.getcwd())
+
+
+def moveDown(currentDir):
+    subdirectory_name = input(ru.CATALOG_NAME)
+    if not subdirectory_name or not os.path.isdir(currentDir) or os.path.exists(currentDir):
+        print(ru.TRY_AGAIN)
+        return
+    if not os.path.exists(os.path.join(currentDir, subdirectory_name)) or not os.path.isdir(os.path.join(currentDir, subdirectory_name)):
+        print(ru.NOT_FIND)
+        return
+    else:
+        os.chdir(os.path.join(currentDir, subdirectory_name))
+        print(ru.NEW_NAME, os.getcwd())
+
+
+def findFiles(directory, target):
+    file_directory = []
+
+    if os.path.isfile(directory) and target in os.path.basename(directory):
+        file_directory.append(directory)
+
+    if os.path.isdir(directory):
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if target in file:
+                    file_directory.append(os.path.join(root, file))
+
+    if len(file_directory) == 0:
+        print(ru.NOT_FIND)
+    else:
+        return file_directory
+
+
 def run_command(command):
     if command == 1:
         print(*os.listdir())
     if command == 2:
-        move_up()
+        moveUp()
     if command == 3:
         direct = input()
-        move_down(direct)
+        moveDown(direct)
     if command == 4:
         route = input()
         count_files(route)
@@ -19,7 +68,7 @@ def run_command(command):
     if command == 6:
         route = input()
         aim = input()
-        find_files(aim, route)
+        findFiles(aim, route)
 
 
 def count_files(path):
@@ -57,7 +106,7 @@ def main():
         QUIT = 7
         print(os.getcwd())
         print(ru.MENU)
-        command = accept_command()
+        command = acceptCommand()
         run_command(command)
         if command == QUIT:
             print('Работа программы завершена.')
